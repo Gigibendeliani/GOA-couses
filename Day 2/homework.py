56bfbf6740c592f31c5348a4549b15b778127ba0
Gigibendeliani <2009svani@gmail.com>
2024-02-08 20:03:14 +0400

    second commit

---
+++ Day 2/homework.py
@@ -0,0 +1,5 @@
+print("I followed  low budget stories")
+
+print("I met and made friends:Jeko Tarieladze,Nilolas Dvali,Mishiko Berelidze,Aleqsandre Gomidzeand and etc.")
+
+print("In my opinion, Goa is a programming academy where they teach you not only programming, but also how to achieve your goals and prepare you for the future.")
