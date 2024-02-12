5b645ab86122637773cf45d999cff646b3ed9964
Gigibendeliani <2009svani@gmail.com>
2024-02-12 15:48:09 +0400

    second comimit

---
+++ Day 3/Homework.py
@@ -0,0 +1,17 @@
+#we need to print our family names
+
+#Me
+
+print("Gigi Bendeliani")
+
+#Sister
+
+print("Anamaria Bendeliani")
+
+#Dad
+
+print("Giorgi Bendeliani")
+
+#Mom
+
+print("Qristina Buliskeria")
